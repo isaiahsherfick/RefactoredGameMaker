c66d226097184ef82f81973489e8b9f7a4953673
Christian Dummer <cjdummer@iu.edu>
2021-09-26 10:16:54 -0400

    Strategy skeletons

---
+++ src/main/java/strategies/BlowUp.java
@@ -0,0 +1,24 @@
+package strategies;
+
+import javafx.geometry.Point2D;
+import javafx.scene.media.Media;
+
+public class BlowUp implements Strategy{
+
+	private Media soundEffect;
+	
+	public Point2D runMoveBehavior() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void attachSound(Media sound) {
+		this.soundEffect = sound;
+	}
+
+}
