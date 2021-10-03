f9a95ddfe7c49fdaa6595356492f59434c53ea0d
Abhishek Dharmendra Tiwari <abtiwa@iu.edu>
2021-10-03 12:20:46 -0400

    Adding upload sprite button

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,50 +1,158 @@
 package controller;
 
-import javafx.scene.control.TextField;
-import javafx.scene.control.TextFormatter;
-import model.MainModel;
-import view.MainView;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import javafx.event.EventHandler;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import layout.CustomLayout;
+import layout.FormLayouts;
 
 public class MainController {
+	
+	private static double orgSceneX, orgSceneY;
+	private static double orgTranslateX, orgTranslateY;
+	
+	private static CustomLayout customLayout;
+	private static Button currentEnabled;
+	
+	private static Stage primaryStage;
+	private static Canvas gameCanvas;
 
-	//Model
-    MainModel account;
+	private static final List<Button> BUTTON_LIST = new ArrayList<>();
+	private static final String CLICKED_BUTTON_STYLE = "-fx-background-color: lightblue; -fx-font-size:17";
+	private static final String NORMAL_BUTTON_STYLE = "-fx-background-color: blue; -fx-font-size:17";
+	
+	private static final HashMap<String, GridPane> BUTTON_FORM_MAP = new HashMap<>();
     
-    public MainController(MainView view){
-        setView(view);
+    public static void start(Stage primaryStage) {
+
+    	MainController.primaryStage = primaryStage;
+		EventsButtonController eventsController = new EventsButtonController();
+		instantiateButtonFormMap();
+		
+		Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
+		
+		gameCanvas = new Canvas(primScreenBounds.getWidth()/2, primScreenBounds.getHeight());
+
+		BorderPane pane = new BorderPane();
+		FlowPane gameFlow = new FlowPane();
+
+		customLayout = new CustomLayout(30,10);
+		customLayout.addNewChild(createText("Maker"), 3, 8);
+//		customLayout.addNewChild(createButton("Sprites"), 0, 14);
+		customLayout.addNewChild(createButton("Sprites"), 1, 14);
+		customLayout.addNewChild(createButton("Actions"), 2, 14);
+		customLayout.addNewChild(createButton("Events"), 3, 14);
+		customLayout.addNewChild(createButton("Sounds"), 4, 14);
+		customLayout.addNewChild(createButton("Stages"), 5, 14);
+		customLayout.addNewChild(createButton("Save"), 6, 14);
+
+		gameFlow.getChildren().add(gameCanvas);
+		gameFlow.setStyle("-fx-background-color: black");
+
+		pane.setLeft(customLayout);
+		pane.setRight(gameFlow);
+
+		// Build scene and give it root
+		Scene gameScene = new Scene(pane, primScreenBounds.getWidth(), primScreenBounds.getHeight());
+		
+		primaryStage.setScene(gameScene);
+		primaryStage.setTitle("Welcome to the Breakout game");
+		primaryStage.show();
+    
     }
-    public void setView(MainView view){
-        //get model
-        account = new MainModel();
-        //link Model with View
-//        view.getAccountHolderDetailsLabel().textProperty().bind(account.accountHolderProperty());
-//        view.getAccountBalanceDetailsLabel().textProperty().bind(account.accountBalanceProperty().asString());
-//        view.getAccountNumberDetailsLabel().textProperty().bind(account.accountNumberProperty().asString());
-        //link Controller to View - ensure only numeric input (integers) in text field
-        view.getAmountTextField().setTextFormatter(new TextFormatter<>(change -> {
-            if (change.getText().matches("\\d+") || change.getText().equals("")) {
-                return change;
-            } else {
-                change.setText("");
-                change.setRange(
-                        change.getRangeStart(),
-                        change.getRangeStart()
-                );
-                return change;
-            }
-        }));
-        //link Controller to View - methods for buttons
-        view.getDepositButton().setOnAction(event -> {
-            account.deposit(getAmount(view.getAmountTextField()));
-            event.consume();
-        });
-        view.getWithdrawalButton().setOnAction(event -> {
-            account.withdraw(getAmount(view.getAmountTextField()));
-            event.consume();
-        });
-    }
-    private double getAmount(TextField field){
-        if (field.getText().equals("")) return 0;
-        return Double.parseDouble(field.getText());
-    }
+    
+    private static void instantiateButtonFormMap() {
+    	BUTTON_FORM_MAP.put("Sprites", FormLayouts.getShapeFormLayout(getPrimaryStage()));
+    	BUTTON_FORM_MAP.put("Events", FormLayouts.getEventsFormLayout());
+    	BUTTON_FORM_MAP.put("Sounds", FormLayouts.getSoundsFormLayout());
+	}
+    
+    private static GridPane createButton(String actionType) {
+		GridPane gridPane = new GridPane();
+		Button button = new Button(actionType);
+		button.setOnAction(event -> {
+			setEnableCurrentButton(button);
+			customLayout.addNewChildPane(BUTTON_FORM_MAP.get(button.getText()), 0, 15);
+		});
+		
+		button.setOnMousePressed(buttonMousePressedEventHandler);
+        button.setOnMouseDragged(buttonOnMouseDraggedEventHandler);
+        button.setStyle(NORMAL_BUTTON_STYLE);
+        button.setTextFill(Color.WHITE);
+		button.setFocusTraversable(false);
+		
+		BUTTON_LIST.add(button);
+
+		gridPane.add(button, 1, 0);
+
+		return gridPane;
+	}
+
+	private static void setEnableCurrentButton(Button button) {
+		if (currentEnabled != null) currentEnabled.setStyle(NORMAL_BUTTON_STYLE);
+		currentEnabled = button;
+		button.setStyle(CLICKED_BUTTON_STYLE);
+		
+	}
+
+	private static GridPane createText(String actionType) {
+		GridPane gridPane = new GridPane();
+		Text text = new Text();
+		text.setText(actionType);
+		text.setStyle("-fx-font-size: 30");
+		gridPane.setStyle("-fx-background-color: orange");
+
+		gridPane.add(text, 1, 0);
+
+		return gridPane;
+	}
+    
+    static EventHandler<MouseEvent> buttonMousePressedEventHandler = new EventHandler<MouseEvent>() {
+
+	    @Override
+	    public void handle(MouseEvent t) {
+	        orgSceneX = t.getSceneX();
+	        orgSceneY = t.getSceneY();
+	        orgTranslateX = ((Button) (t.getSource())).getTranslateX();
+	        orgTranslateY = ((Button) (t.getSource())).getTranslateY();
+
+	        ((Button) (t.getSource())).toFront();
+	    }
+	};
+
+	static EventHandler<MouseEvent> buttonOnMouseDraggedEventHandler = new EventHandler<MouseEvent>() {
+
+	    @Override
+	    public void handle(MouseEvent t) {
+	        double offsetX = t.getSceneX() - orgSceneX;
+	        double offsetY = t.getSceneY() - orgSceneY;
+	        double newTranslateX = orgTranslateX + offsetX;
+	        double newTranslateY = orgTranslateY + offsetY;
+
+	        ((Button) (t.getSource())).setTranslateX(newTranslateX);
+	        ((Button) (t.getSource())).setTranslateY(newTranslateY);
+	    }
+	};
+	
+	public static Canvas getGameCanvas() {
+		return gameCanvas;
+	}
+
+	public static Stage getPrimaryStage() {
+		return primaryStage;
+	}
 }
--- src/main/java/layout/FormLayouts.java
+++ src/main/java/layout/FormLayouts.java
@@ -1,5 +1,6 @@
 package layout;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -15,15 +16,19 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceDialog;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import javafx.stage.Window;
 import javafx.util.StringConverter;
 import sound.Sound;
@@ -35,8 +40,8 @@ public class FormLayouts {
 		return createSpriteFormPane();
 	}
 	
-	public static GridPane getShapeFormLayout() {
-		return createShapeFormPane();
+	public static GridPane getShapeFormLayout(Stage primaryStage) {
+		return createShapeFormPane(primaryStage);
 	}
 	
 	public static GridPane getEventsFormLayout() {
@@ -54,8 +59,8 @@ public class FormLayouts {
 
 	}
 	
-	private static GridPane createShapeFormPane() {
-		return addShapeUI(getGridPane());
+	private static GridPane createShapeFormPane(Stage primaryStage) {
+		return addShapeUI(getGridPane(), primaryStage);
 	}
 	
 	private static GridPane createEventFormPane() {
@@ -137,7 +142,7 @@ public class FormLayouts {
         return gridPane;
 	}
 	
-	private static GridPane addShapeUI(GridPane gridPane) {
+	private static GridPane addShapeUI(GridPane gridPane, Stage primaryStage) {
 		// Add Header
         Label headerLabel = new Label("Shape Form");
         headerLabel.setFont(Font.font("Arial", FontWeight.BOLD, 24));
@@ -149,13 +154,25 @@ public class FormLayouts {
         Label nameLabel = new Label("Select Shape : ");
         gridPane.add(nameLabel, 0,1);
         
-        ComboBox shapes = new ComboBox();
-        shapes.getItems().addAll(
-                "Square",
-                "Circle",
-                "Rectangle"
-            );
-        gridPane.add(shapes, 1, 1);
+        //ChoiceDialog for the user to select the default shapes
+        ChoiceDialog<String> customisedInputDialog = new ChoiceDialog<String>("SQUARE", "CIRCLE", "RECTANGLE");
+
+        gridPane.add((GridPane) customisedInputDialog.getDialogPane().getContent(), 1, 1);
+        
+        FileChooser fileChooser = new FileChooser();
+
+        Button openButton = new Button("Choose Image");
+        openButton.setOnAction((final ActionEvent e) -> {
+            File file = fileChooser.showOpenDialog(primaryStage);
+            if (file != null) {
+               Image image = new Image(file.toURI().toString());
+            }
+        });
+        
+        Label orLabel = new Label("OR");
+        gridPane.add(orLabel, 2,1);
+        
+        gridPane.add(openButton, 3, 1);
         
         // Add Submit Button
         Button submitButton = new Button("Save");
