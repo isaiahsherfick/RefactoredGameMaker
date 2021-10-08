6aec010a702ecb221e056c04628e98bf1c3f7895
Isaiah Sherfick <isherfic@iu.edu>
2021-10-08 11:56:34 -0400

    Create tests for new model functionality, update tickets

---
+++ src/test/java/ModelTests.java
@@ -0,0 +1,30 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ModelTests {
+
+	@Test
+	//Test to ensure that the sprite manager is correctly modifying the sprite IDs upon insertion
+	public void SpriteManagerTest()
+	{
+//		SpriteManager spriteManager = new SpriteManager();
+//		sprite1 = new Sprite();
+//		sprite2 = new Sprite();
+//		sprite3 = new Sprite();
+//		sprite4 = new Sprite();
+//		assertEquals(Constants.DEFAULT_SPRITE_ID, sprite1.getSpriteID());
+//		assertEquals(Constants.DEFAULT_SPRITE_ID, sprite2.getSpriteID());
+//		assertEquals(Constants.DEFAULT_SPRITE_ID, sprite3.getSpriteID());
+//		assertEquals(Constants.DEFAULT_SPRITE_ID, sprite4.getSpriteID());
+//		spriteManager.add(sprite1);
+//		spriteManager.add(sprite2);
+//		spriteManager.add(sprite3);
+//		spriteManager.add(sprite4);
+//		assertEquals(0, sprite1.getSpriteID());
+//		assertEquals(1, sprite2.getSpriteID());
+//		assertEquals(2, sprite3.getSpriteID());
+//		assertEquals(3, sprite4.getSpriteID());
+	}
+
+}
