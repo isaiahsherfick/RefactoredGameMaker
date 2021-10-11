fe95cc0979a95dcb8947dff96a664578bae7ec79
Christian Dummer <cjdummer@iu.edu>
2021-10-11 14:32:52 -0400

    Created PlayerView and Show it

---
+++ src/main/java/views/View.java
@@ -0,0 +1,45 @@
+//@Author Christian Dummer
+package views;
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class View {
+		//Displays both views
+		public static void start(Stage makerStage) {
+			try {
+				//Loads and shows the makerView
+				makerStage.setTitle("Maker View");
+				FXMLLoader windowLoader = new FXMLLoader();
+				windowLoader.setLocation(View.class.getResource("MakerView.fxml"));
+				AnchorPane makerLayout = (AnchorPane)windowLoader.load();
+				Scene makerScene = new Scene(makerLayout);
+				makerStage.setScene(makerScene);
+				makerStage.setX(300);
+				makerStage.setY(50);
+				makerStage.show();
+				
+				//Loads and shows the playerView
+				Stage playerStage = new Stage();
+				playerStage.setTitle("Player View");
+				FXMLLoader playerWindowLoader = new FXMLLoader();
+				playerWindowLoader.setLocation(View.class.getResource("PlayerView.fxml"));
+				AnchorPane playerLayout = (AnchorPane)playerWindowLoader.load();
+				Scene playerScene = new Scene(playerLayout);
+				playerStage.setScene(playerScene);
+				playerStage.setX(900);
+				playerStage.setY(50);
+				playerStage.show();
+
+				
+			}
+			catch(IOException ex) {
+				System.out.println(ex);
+			}
+			
+		}
+}
