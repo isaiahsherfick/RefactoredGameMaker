afa43a6b99fd3809939b383d1b84de6e012e48ff
ramya <rramsam@iu.edu>
2021-10-12 10:13:44 -0400

    Command Design Pattern

--- src/main/java/controller/MakeGameController.java
+++ src/main/java/controller/MakeGameController.java
@@ -2,16 +2,15 @@ package controller;
 
 import java.io.IOException;
 
+import command.CreateSpriteCommand;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
+//import javafx.scene.Scene;
 
 public class MakeGameController {
 
 	    private Parent makerview; //the view
-	    private Scene scene;
 	    public static final String FILE_PATH = "makerview.fxml";
 
 
@@ -21,29 +20,23 @@ public class MakeGameController {
 	     *              the buttons ribbon/container can be added there.
 	     */
 	    
-	    public MakeGameController(Scene scene)
+	    public MakeGameController()
 	    {
-	        this.scene = scene;
 	        try
 	        {
 	            //1. Create the game maker view
 	            makerview = FXMLLoader.load(getClass().getClassLoader().getResource(FILE_PATH));
-	            //2. Select the properties VBox from the scene
-	            VBox vboxPreview = (VBox) scene.lookup("#vboxPreview");
-	            //3. Add the ribbon to the editor scene
-	            vboxPreview.getChildren().add(0, makerview);
+	           // TODO 
 	        }
 	        catch (IOException e) {
 	            System.out.println("Failed to load in " + FILE_PATH);
 	            e.printStackTrace();
 	        }
-	    }
-
-	    /**
-	     * Required constructor for when the XML attaches itself to this
-	     */
-	    public MakeGameController() {
-	    }
+	    }  
+	    
+	    	public void onClickCreateSprite(ActionEvent event)
+	    	{
+	    	}
 
 
 }
