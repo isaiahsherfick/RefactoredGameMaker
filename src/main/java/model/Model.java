93e352afdc712790d1d770178b75acc6917dffac
Isaiah Sherfick <isherfic@iu.edu>
2021-10-17 12:22:10 -0400

    Added custom savefilepath APIs for Christian

--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -101,6 +101,39 @@ public class Model
 		saveAndLoadManager.addAll(saveableList);
 		saveAndLoadManager.saveFile(saveFilePath);
 	}
+
+	//Save all sprites, write them to the file stored at saveFilePath
+	//Catch the exception in controller
+	public void saveToFile(String filePath) throws IOException
+	{
+		resetSaveAndLoadManager();
+		ArrayList<Sprite> spriteList = spriteManager.getSpriteList();
+		ArrayList<Saveable> saveableList = new ArrayList<>();
+		for (Sprite s : spriteList)
+		{
+			saveableList.add((Saveable)s);
+		}
+		saveAndLoadManager.addAll(saveableList);
+		saveAndLoadManager.saveFile(filePath);
+	}
+	
+	//Load all sprites from the file at saveFilePath
+	//Catch the exceptions in controller
+	//IOExeption : file can't be found
+	//ParseException : JSON is bad
+	public void loadFromFile(String filePath) throws IOException, ParseException
+	{
+		resetSaveAndLoadManager();
+		saveAndLoadManager.loadFile(filePath);
+		ArrayList<Sprite> spriteList = saveAndLoadManager.getSprites();
+//		for (int i = 0; i<spriteList.size(); i++)
+//		{
+//			System.out.println(spriteList.get(i));
+//		}
+		resetSpriteManager();
+		spriteManager.addAll(spriteList);
+		notifyObservers();
+	}
 	
 	//stash the model as a JSON - will be called when the controller switches contexts
 	public JSONObject stash()
@@ -143,7 +176,6 @@ public class Model
 		resetSpriteManager();
 		spriteManager.addAll(spriteList);
 		notifyObservers();
-		//TODO this needs to maintain image on load, not just shape
 	}
 	
 	public String getSaveFilePath()
